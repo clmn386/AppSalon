e90d6a833deda57ce69c8be1c36f2beb57a75118
clmn386 <clmn386@gmail.com>
2022-05-12 22:03:10 +0200

    router

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,12 +2,25 @@
 
 namespace Controllers;
 
-class LoginController {
+class LoginController 
+{
     public static function login() {
         echo "Desde Login";
     }
     
     public static function logout() {
         echo "Desde Logout";
+    }
+
+    public static function olvide() {
+        echo "olvide password";
+    }
+
+    public static function recovery() {
+        echo "Recuperando password";
+    }
+
+    public static function crear() {
+        echo "crear cuenta";
     }
 }
--- public/index.php
+++ public/index.php
@@ -13,8 +13,15 @@ $router->post('/', [LoginController::class, 'login']);
 $router->get('/logout', [LoginController::class, 'logout']);
 
 // recuperar  Password
+$router->get('/olvide', [LoginController::class, 'olvide']);
+$router->post('/olvide', [LoginController::class, 'olvide']);
 $router->get('/recovery', [LoginController::class, 'recovery']);
 $router->post('/recovery', [LoginController::class, 'recovery']);
+
+//Crear cuenta
+$router->get('/crear', [LoginController::class, 'crear']);
+$router->post('/crear', [LoginController::class, 'crear']);
+
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
