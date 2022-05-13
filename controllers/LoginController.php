c35a5f5bbc31d16845fe3b6a7d397c387b5c49c3
clmn386 <clmn386@gmail.com>
2022-05-13 21:13:26 +0200

    .

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,10 +2,14 @@
 
 namespace Controllers;
 
+use MVC\Router;
+
 class LoginController 
 {
-    public static function login() {
-        echo "Desde Login";
+    public static function login(Router $router) {
+        
+        $router->render('auth/login');
+
     }
     
     public static function logout() {
